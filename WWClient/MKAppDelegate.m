61d4d6e6402821d670df752bc26d64a87bb0ec8b
Moonko <moonko@me.com>
2014-04-20 13:41:00 +0400

    Revert f015961..5708b1c
    This rolls back to commit f01596125817c68ad3677ff0db64ce298a67c94b.

--- WWClient/MKAppDelegate.m
+++ WWClient/MKAppDelegate.m
@@ -15,7 +15,14 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    
+    SKScene *scene = [MKMainMenuScene sceneWithSize:CGSizeMake(1024, 768)];
+
+    scene.scaleMode = SKSceneScaleModeAspectFit;
+
+    [self.skView presentScene:scene];
+
+    self.skView.showsFPS = YES;
+    self.skView.showsNodeCount = YES;
 }
 
 - (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender
